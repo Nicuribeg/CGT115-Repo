7fb8e2e6fc81b95c57b410aa0ec50191d58975d5
Nicuribeg <nicuribeg@proton.me>
2026-04-25 17:26:52 -0400

    Update main.py

--- Gunfight/main.py
+++ Gunfight/main.py
@@ -10,12 +10,12 @@ TestDud = "REPLACE ME"
 #constants and globals
 score1=0
 score2=0
-playermove = 0.5
+playermove = 0.075
 done = False
 
 #timers
 def gametimer(seconds):
-    dud = "1"
+    TestDud
 
 
 #collisions
@@ -26,7 +26,7 @@ COLLTYPE_PLAYER = 4
 COLLTYPE_OBSTACLE = 5
 # PLAYER FUNCTIONS
 
-# Player 1 Mover
+#Player 1 Mover
 def MovePlayer1(body, shape, left, right, up, down):
     deltaX = 0
     deltaY = 0
@@ -97,6 +97,7 @@ space.add(circleBody1, circleShape1)
 circleBody2 = pymunk.Body( 1, 100)
 circleBody2.position = (500, 300)
 circleShape2 = pymunk.Circle(circleBody2, 10)
+circleShape2.filter = pymunk.ShapeFilter(group=2)
 space.add(circleBody2, circleShape2)
 
 #bullet creations
@@ -111,14 +112,32 @@ def fireLeftBullet(gameSpace,player_body):
     bulletShape = pymunk.Circle(bulletBody, 5)
     bulletShape.friction = 0
     bulletShape.filter = (pymunk.ShapeFilter(group=1))
-    bulletBody.velocity = (800, 0)
 
     gameSpace.add(bulletBody, bulletShape)
+    bulletBody.velocity = (5, 0)
+
+    return bulletBody
+
+def fireRightBullet(gameSpace,player_body):
+    bulletSpawnX = player_body.position.x - 35
+    bulletSpawnY = player_body.position.y
+    bulletMass = 1
+    bulletMoment = pymunk.moment_for_circle(bulletMass,0,5)
+    bulletBody = pymunk.Body(bulletMass, bulletMoment)
+    spawnDistance = 35
+    bulletBody.position = (bulletSpawnX + spawnDistance, bulletSpawnY)
+    bulletShape = pymunk.Circle(bulletBody, 5)
+    bulletShape.friction = 0
+    bulletShape.filter = (pymunk.ShapeFilter(group=2))
 
 
-    return bulletShape
+    gameSpace.add(bulletBody, bulletShape)
+    bulletBody.velocity = (-5, 0)
 
+    return bulletBody
 
+bullets1 = []
+bullets2 = []
 #Game Loop
 leftArrowDown = False
 rightArrowDown = False
@@ -128,8 +147,15 @@ AKeyDown = False
 DKeyDown = False
 WKeyDown = False
 SKeyDown = False
-LeftShiftDown = False
+#LeftShiftDown = False
 RightShiftDown = False
+
+roundActive = False
+ammo1 = 6
+ammo2 = 6
+
+out_of_ammo1 = False
+out_of_ammo2 = False
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -153,7 +179,15 @@ while not done:
             if event.key == pygame.K_s:
                 SKeyDown = True
             if event.key == pygame.K_LSHIFT:
-                LeftShiftDown = True
+                if out_of_ammo1 == False:
+                    newBullet = fireLeftBullet(space, circleBody1)
+                    bullets1.append(newBullet)
+                    ammo1 = ammo1 - 1
+            if event.key == pygame.K_RSHIFT:
+                if out_of_ammo2 == False:
+                    newBullet = fireRightBullet(space, circleBody2)
+                    bullets2.append(newBullet)
+                    ammo2 = ammo2 - 1
             if event.key == pygame.K_RSHIFT:
                 RightShiftDown = True
         if event.type == pygame.KEYUP:
@@ -173,32 +207,30 @@ while not done:
                 downArrowDown = False
             if event.key == pygame.K_s:
                 SKeyDown = False
-            if event.key == pygame.K_LSHIFT:
-                LeftShiftDown = False
-            if event.key == pygame.K_RSHIFT:
-                RightShiftDown = False
-
-    #Keeps track of bullets active
-    bullets = []
-    if LeftShiftDown:
-        newBullet = fireLeftBullet(space,circleBody1)
-        bullets.append(newBullet)
 
 
 
     MovePlayer1(circleBody1, circleShape1, leftArrowDown, rightArrowDown, downArrowDown, upArrowDown)
     space.step(1 / 60.0)
     screen.fill((0, 0, 0))
-    circleX = int(circleBody1.position.x)
-    circleY = int(circleBody1.position.y)
-    pygame.draw.circle(screen, (255, 255, 255), (circleX, circleY), 10)
-    #MovePlayer2(circleBody2, circleShape2, AKeyDown, DKeyDown, SKeyDown, WKeyDown)
-    #circleX = int(circleBody2.position.x)
-    #circleY = int(circleBody2.position.y)
-    #pygame.draw.circle(screen, (255, 255, 255), (circleX, circleY), 10)
+    circle1X = int(circleBody1.position.x)
+    circle1Y = int(circleBody1.position.y)
+    pygame.draw.circle(screen, (255, 255, 255), (circle1X, circle1Y), 10)
+    MovePlayer2(circleBody2, circleShape2, AKeyDown, DKeyDown, SKeyDown, WKeyDown)
+    circle2X = int(circleBody2.position.x)
+    circle2Y = int(circleBody2.position.y)
+    pygame.draw.circle(screen, (255, 255, 255), (circle2X, circle2Y), 10)
 
-    for bullet in bullets:
-        pos = bullet.body.position
-        pygame.draw.circle(screen, (255, 0, 0), (int(pos.x), int(pos.y)), 5)
-
+    for bullet in bullets1:
+        pos = bullet.position
+        pygame.draw.circle(screen, (255, 0, 0), (int(pos.x), int(pos.y)), 10)
+    for bullet in bullets2:
+        pos = bullet.position
+        pygame.draw.circle(screen, (255, 0, 0), (int(pos.x), int(pos.y)), 10)
     pygame.display.update()
+
+    if ammo1 <= 0:
+        out_of_ammo1 = True
+    if ammo2 <= 0:
+        out_of_ammo2 = True
+
